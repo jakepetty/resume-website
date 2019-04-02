dcd4f218946f86793b3cf0ca6b8b797af79682ca
Jake Petty <support@vexcon.net>
2019-04-02 16:29:23 -0700

    Updated backend layout

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -21,21 +21,16 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+        <nav class="navbar navbar-expand-md navbar-dark">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }} <small>Administration</small>
+                    Résumé <small>Administration</small>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
@@ -50,30 +45,19 @@
                             @endif
                         @else
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('skills.index') }}">{{ __('Skills') }}</a>
+                            <a class="nav-link" href="{{ route('skills.index') }}"><i class="fas fa-graduation-cap"></i> {{ __('Skills') }}</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('projects.index') }}">{{ __('Projects') }}</a>
+                            <a class="nav-link" href="{{ route('projects.index') }}"><i class="fab fa-github"></i> {{ __('Projects') }}</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('tools.index') }}">{{ __('Tools') }}</a>
+                            <a class="nav-link" href="{{ route('tools.index') }}"><i class="fas fa-code"></i> {{ __('Tools') }}</a>
                         </li>
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
+                            <li class="nav-item">
+                                <form id="logout-form" class="form-inline" action="{{ route('logout') }}" method="POST">
+                                    @csrf
+                                    <button class="btn btn-outline-light"><i class="fas fa-sign-out-alt"></i> Logout</button>
+                                </form>
                             </li>
                         @endguest
                     </ul>
