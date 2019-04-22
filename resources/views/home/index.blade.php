ce8ef3adbcf2ddf42f1e68a5418a76d38655d5dc
Jake Petty <support@vexcon.net>
2019-04-22 12:08:20 -0700

    Fixed responsiveness issue on small screen devices

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -38,137 +38,138 @@
         </ul>
     </div>
 </nav>
+<div class="container-fluid">
+    <section id="skills" class="container">
+        <div class="heading">
+            <h2><span>{{ __('Pro') }}</span>{{ __('Skills') }}</h2>
+            <p class="lead">{{ __('I am proficient in the following') }}</p>
+        </div>
+        <div class="row row-eq-height">
+            <div class="col-md-6 col-lg-3 mb-5">
+                <div class="card h-100">
+                    <div class="card-header">
+                        @auth
+                        <a class="manage" href="{{ route('languages.index') }}" data-toggle="tooltip" data-placement="top" title="{{ __('Manage') }}"><i class="fas fa-edit"></i></a>
+                        @endauth
+                        <i class="icon fas fa-code"></i> {{ __('Languages') }}
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        @foreach($languages as $language)
+                        <li class="list-group-item">{{ $language->name }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3 mb-5">
+                <div class="card h-100">
+                    <div class="card-header">
+                        <i class="icon fas fa-project-diagram"></i> {{ __('Frameworks') }}
+                        @auth
+                        <a class="manage" href="{{ route('frameworks.index') }}" data-toggle="tooltip" data-placement="top" title="{{ __('Manage') }}"><i class="fas fa-edit"></i></a>
+                        @endauth
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        @foreach($frameworks as $framework)
+                        <li class="list-group-item"><a href="{{ $framework->url }}">{{ $framework->name }}</a></li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3 mb-5">
+                <div class="card h-100">
+                    <div class="card-header">
+                        @auth
+                        <a class="manage" href="{{ route('applications.index') }}" data-toggle="tooltip" data-placement="top" title="{{ __('Manage') }}"><i class="fas fa-edit"></i></a>
+                        @endauth
+                        <i class="icon fas fa-laptop-code"></i> {{ __('Applications') }}
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        @foreach($applications as $application)
+                        <li class="list-group-item"><a href="{{ $application->url }}">{{ $application->name }}</a></li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3 mb-5">
+                <div class="card h-100">
+                    <div class="card-header">
+                        <i class="icon fas fa-server"></i> Servers
+                        @auth
+                        <a class="manage" href="{{ route('servers.index') }}" data-toggle="tooltip" data-placement="top" title="{{ __('Manage') }}"><i class="fas fa-edit"></i></a>
+                        @endauth
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        @foreach($servers as $server)
+                        <li class="list-group-item"><a href="{{ $server->url }}">{{ $server->name }}</a></li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
 
-<section id="skills" class="container">
-    <div class="heading">
-        <h2><span>{{ __('Pro') }}</span>{{ __('Skills') }}</h2>
-        <p class="lead">{{ __('I am proficient in the following') }}</p>
-    </div>
-    <div class="row row-eq-height">
-        <div class="col-md-6 col-lg-3 mb-5">
-            <div class="card h-100">
-                <div class="card-header">
-                    @auth
-                    <a class="manage" href="{{ route('languages.index') }}" data-toggle="tooltip" data-placement="top" title="{{ __('Manage') }}"><i class="fas fa-edit"></i></a>
-                    @endauth
-                    <i class="icon fas fa-code"></i> {{ __('Languages') }}
-                </div>
-                <ul class="list-group list-group-flush">
-                    @foreach($languages as $language)
-                    <li class="list-group-item">{{ $language->name }}</li>
-                    @endforeach
-                </ul>
-            </div>
+    <section id="projects" class="container">
+        <div class="heading">
+            <h2><span>{{ __('Work') }}</span>{{ __('Portfolio') }}</h2>
+            <p class="lead">{{ __("A small collection of projects I've worked on") }}</p>
         </div>
-        <div class="col-md-6 col-lg-3 mb-5">
-            <div class="card h-100">
-                <div class="card-header">
-                    <i class="icon fas fa-project-diagram"></i> {{ __('Frameworks') }}
-                    @auth
-                    <a class="manage" href="{{ route('frameworks.index') }}" data-toggle="tooltip" data-placement="top" title="{{ __('Manage') }}"><i class="fas fa-edit"></i></a>
-                    @endauth
-                </div>
-                <ul class="list-group list-group-flush">
-                    @foreach($frameworks as $framework)
-                    <li class="list-group-item"><a href="{{ $framework->url }}">{{ $framework->name }}</a></li>
-                    @endforeach
-                </ul>
-            </div>
-        </div>
-        <div class="col-md-6 col-lg-3 mb-5">
-            <div class="card h-100">
-                <div class="card-header">
-                    @auth
-                    <a class="manage" href="{{ route('applications.index') }}" data-toggle="tooltip" data-placement="top" title="{{ __('Manage') }}"><i class="fas fa-edit"></i></a>
-                    @endauth
-                    <i class="icon fas fa-laptop-code"></i> {{ __('Applications') }}
-                </div>
-                <ul class="list-group list-group-flush">
-                    @foreach($applications as $application)
-                    <li class="list-group-item"><a href="{{ $application->url }}">{{ $application->name }}</a></li>
-                    @endforeach
-                </ul>
-            </div>
-        </div>
-        <div class="col-md-6 col-lg-3 mb-5">
-            <div class="card h-100">
-                <div class="card-header">
-                    <i class="icon fas fa-server"></i> Servers
-                    @auth
-                    <a class="manage" href="{{ route('servers.index') }}" data-toggle="tooltip" data-placement="top" title="{{ __('Manage') }}"><i class="fas fa-edit"></i></a>
-                    @endauth
-                </div>
-                <ul class="list-group list-group-flush">
-                    @foreach($servers as $server)
-                    <li class="list-group-item"><a href="{{ $server->url }}">{{ $server->name }}</a></li>
-                    @endforeach
-                </ul>
-            </div>
-        </div>
-    </div>
-</section>
-
-<section id="projects" class="container">
-    <div class="heading">
-        <h2><span>{{ __('Work') }}</span>{{ __('Portfolio') }}</h2>
-        <p class="lead">{{ __("A small collection of projects I've worked on") }}</p>
-    </div>
-    <div class="row">
-        @foreach($projects as $project)
-        <div class="col-md-6 col-lg-5 col-xl-4 d-flex">
-            <div class="card flex-fill">
-                <div class="card-img-top" style="background-image: url(images/projects/{{ $project->github_id ? $project->github_id : $project->id }}.jpg)">
-                    <div class="hover-link row h-100 justify-content-center align-items-center">
-                        <div class="btn-group">
-                            @if($project->demo)<a href="{{ $project->demo }}" class="btn btn-blue" target="_blank"><i class="fas fa-eye"></i> {{ __('Demo') }}</a>@endif
-                            <a href="{{ $project->url }}" target="blank" class="btn btn-blue float-right"><i class="fas fa-code"></i> {{ __('Code') }}</a>
+        <div class="row">
+            @foreach($projects as $project)
+            <div class="col-md-6 col-lg-5 col-xl-4 d-flex">
+                <div class="card flex-fill">
+                    <div class="card-img-top" style="background-image: url(images/projects/{{ $project->github_id ? $project->github_id : $project->id }}.jpg)">
+                        <div class="hover-link row h-100 justify-content-center align-items-center">
+                            <div class="btn-group">
+                                @if($project->demo)<a href="{{ $project->demo }}" class="btn btn-blue" target="_blank"><i class="fas fa-eye"></i> {{ __('Demo') }}</a>@endif
+                                <a href="{{ $project->url }}" target="blank" class="btn btn-blue float-right"><i class="fas fa-code"></i> {{ __('Code') }}</a>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="card-body">
-                    <h5 class="card-title">{{ $project->name }}</h5>
-                    <p class="card-text">{{ $project->description }}</p>
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $project->name }}</h5>
+                        <p class="card-text">{{ $project->description }}</p>
+                    </div>
                 </div>
             </div>
+            @endforeach
         </div>
-        @endforeach
-    </div>
-    <div class="text-center mt-5">
-        <a href="https://github.com/{{ config('app.github.username') }}" class="btn btn-outline-dark btn-lg"><i class="fab fa-github"></i> {{ __('Visit My GitHub') }}</a>
-    </div>
-</section>
+        <div class="text-center mt-5">
+            <a href="https://github.com/{{ config('app.github.username') }}" class="btn btn-outline-dark btn-lg"><i class="fab fa-github"></i> {{ __('Visit My GitHub') }}</a>
+        </div>
+    </section>
 
-<section id="contact" class="container">
-    <div class="heading">
-        <h2><span>{{ __('Say') }}</span>{{  __('Hello') }}</h2>
-        <p class="lead">{{ __('Have questions or want to hire me? Send me a message!') }}</p>
-    </div>
-    <form action="{{ route('contact.send') }}" method="POST">
-        @csrf
-        <div class="form-row">
-            <div class="form-group col-md-6">
-                <input type="text" class="form-control {{ $errors->has('name') ? 'is-invalid' : ($errors->any() ? 'is-valid' : null) }}" id="contact-name" name="name" value="{{ old('name') }}" placeholder="{{ __('Your Name') }}" required>
-                @if($errors->has('name'))
-                <div class="invalid-feedback">{{ $errors->first('name') }}</div>
+    <section id="contact" class="container">
+        <div class="heading">
+            <h2><span>{{ __('Say') }}</span>{{  __('Hello') }}</h2>
+            <p class="lead">{{ __('Have questions or want to hire me? Send me a message!') }}</p>
+        </div>
+        <form action="{{ route('contact.send') }}" method="POST">
+            @csrf
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <input type="text" class="form-control {{ $errors->has('name') ? 'is-invalid' : ($errors->any() ? 'is-valid' : null) }}" id="contact-name" name="name" value="{{ old('name') }}" placeholder="{{ __('Your Name') }}" required>
+                    @if($errors->has('name'))
+                    <div class="invalid-feedback">{{ $errors->first('name') }}</div>
+                    @endif
+                </div>
+                <div class="form-group col-md-6">
+                    <input type="email" class="form-control {{ $errors->has('email') ? 'is-invalid' : ($errors->any() ? 'is-valid' : null) }}" id="contact-email" name="email" value="{{ old('email') }}" placeholder="{{ __('Your Email') }}" required>
+                    @if($errors->has('email'))
+                    <div class="invalid-feedback">{{ $errors->first('email') }}</div>
+                    @endif
+                </div>
+            </div>
+            <div class="form-group">
+                <textarea name="body" class="form-control {{ $errors->has('body') ? 'is-invalid' : ($errors->any() ? 'is-valid' : null) }}" id="contact-body" placeholder="{{ __('Your Message') }}" required>{{ old('body') }}</textarea>
+                @if($errors->has('body'))
+                <div class="invalid-feedback">{{ $errors->first('body') }}</div>
                 @endif
             </div>
-            <div class="form-group col-md-6">
-                <input type="email" class="form-control {{ $errors->has('email') ? 'is-invalid' : ($errors->any() ? 'is-valid' : null) }}" id="contact-email" name="email" value="{{ old('email') }}" placeholder="{{ __('Your Email') }}" required>
-                @if($errors->has('email'))
-                <div class="invalid-feedback">{{ $errors->first('email') }}</div>
-                @endif
-            </div>
-        </div>
-        <div class="form-group">
-            <textarea name="body" class="form-control {{ $errors->has('body') ? 'is-invalid' : ($errors->any() ? 'is-valid' : null) }}" id="contact-body" placeholder="{{ __('Your Message') }}" required>{{ old('body') }}</textarea>
-            @if($errors->has('body'))
-            <div class="invalid-feedback">{{ $errors->first('body') }}</div>
-            @endif
-        </div>
-        <button class="btn btn-outline-dark"><i class="fas fa-paper-plane"></i> {{ __('Send') }}</button>
-    </form>
-</section>
-<footer> &copy; {{ date('Y') }} {{ config('app.name') }}. {{ __('All Rights Reserved.') }}</footer>
+            <button class="btn btn-outline-dark"><i class="fas fa-paper-plane"></i> {{ __('Send') }}</button>
+        </form>
+    </section>
+    <footer> &copy; {{ date('Y') }} {{ config('app.name') }}. {{ __('All Rights Reserved.') }}</footer>
+</div>
 @if(Session::has('message'))
 <div class="modal" tabindex="-1" role="dialog">
     <div class="modal-dialog" role="document">
