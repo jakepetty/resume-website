fbb42ba73e625e040c20dcd93db9c690c61bffcf
Jake Petty <support@vexcon.net>
2019-05-03 08:26:36 -0700

    Commented some source

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,27 +3,40 @@
 
 @section('content')
 
+<!-- INTRO BEGIN -->
 @component('components.intro')
 @endcomponent
+<!-- INTRO END -->
+<!-- NAVBAR BEGIN -->
 @component('components.navbar')
 @endcomponent
-
+<!-- NAVBAR END -->
+<!-- PROJECTS BEGIN -->
 @component('components.projects', compact('projects'))
 @endcomponent
+<!-- PROJECTS END -->
+<!-- SKILLS BEGIN -->
 @component('components.skills', compact('skills'))
 @endcomponent
+<!-- SKILLS END -->
+<!-- CONTACT BEGIN -->
 @component('components.contact')
 @endcomponent
+<!-- CONTACT END -->
+<!-- FOOTER BEGIN -->
 @component('components.footer')
 @endcomponent
+<!-- FOOTER END -->
 @if(Session::has('message'))
-@component('components.modal')
-@slot('name')
-{{ Session::get('name') }}
-@endslot
-@slot('message')
-{{ Session::get('message') }}
-@endslot
-@endcomponent
+<!-- MODAL BEGIN -->
+    @component('components.modal')
+        @slot('name')
+            {{ Session::get('name') }}
+        @endslot
+        @slot('message')
+            {{ Session::get('message') }}
+        @endslot
+    @endcomponent
+<!-- MODAL END -->
 @endif
 @endsection
--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -10,31 +10,42 @@
     <title>{{ config('app.name') }} | Full-Stack Web Developer</title>
 
     <meta name="author" content="{{ config('app.name') }}">
-    <meta name="description" content="Full-Stack Web Developer &amp; Mentor">
+    <meta name="description" content="Full-Stack Web Developer &amp; mentor. I develop simple but complex projects for businesses and consumers and I love every minute of it.">
     <link rel="canonical" href="{{ config('app.url') }}">
+    <!-- OPEN GRAPH BEGIN -->
     <meta property="og:title" content="{{ config('app.name') }}">
     <meta property="og:locale" content="en_US">
     <meta property="og:type" content="website">
-    <meta property="og:description" content="Full-Stack Web Developer &amp; Mentor. I produce elegantly simple things, and I love what I do.">
+    <meta property="og:description" content="Full-Stack Web Developer. I develop simple but complex projects for businesses and consumers and I love every minute of it.">
     <meta property="og:url" content="{{ config('app.url') }}">
     <meta property="og:site_name" content="{{ config('app.name') }}">
     <meta property="og:image" content="{{ config('app.url') }}/img/og_thumbnail.png">
+    <!-- OPEN GRAPH END -->
+    <!-- TWITTER CARD BEGIN -->
     <meta name="twitter:card" content="summary">
     <meta name="twitter:title" content="{{ config('app.name') }}">
-    <meta name="twitter:description" content="Full-Stack Web Developer &amp; Mentor. I produce elegantly simple things, and I love what I do.">
+    <meta name="twitter:description" content="Full-Stack Web Developer. I develop simple but complex projects for businesses and consumers and I love every minute of it.">
     <meta name="twitter:image" content="{{ config('app.url') }}/img/og_thumbnail.png">
     <meta name="twitter:image:alt" content="{{ config('app.name') }}">
-
+    <!-- TWITTER CARD END -->
+    <!-- CSS BEGIN -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <!-- CSS END -->
+    <!-- ICONS BEGIN -->
     <link rel="shortcut icon" href="favicon.png" type="image/x-icon">
     <link rel="apple-touch-icon" href="favicon.png">
+    <!-- ICONS END -->
 
 </head>
 
 <body data-spy="scroll" data-target=".navbar" data-offset="100">
+    <!-- CONTENT BEGIN -->
     @yield('content')
+    <!-- CONTENT END -->
+    <!-- SCRIPTS BEGIN -->
     <script src="{{ mix('js/app.js') }}"></script>
     @yield('scripts')
+    <!-- SCRIPTS END -->
 </body>
 
 </html>
