255e09163abb7770c39de24d2b6ea508e075d2a2
Jake Petty <support@vexcon.net>
2019-05-03 07:31:59 -0700

    Addressed additional issues with OpenGraph

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -15,14 +15,14 @@
     <meta property="og:title" content="{{ config('app.name') }}">
     <meta property="og:locale" content="en_US">
     <meta property="og:type" content="website">
-    <meta property="og:description" content="Full-Stack Web Developer &amp; Mentor">
+    <meta property="og:description" content="Full-Stack Web Developer &amp; Mentor. I produce elegantly simple things, and I love what I do.">
     <meta property="og:url" content="{{ config('app.url') }}">
     <meta property="og:site_name" content="{{ config('app.name') }}">
     <meta property="og:image" content="{{ config('app.url') }}/img/og_thumbnail.png">
     <meta name="twitter:card" content="summary">
     <meta name="twitter:site" content="">
     <meta name="twitter:title" content="{{ config('app.name') }}">
-    <meta name="twitter:description" content="Full-Stack Web Developer &amp; Mentor. I design and code beautifully simple things, and I love what I do.">
+    <meta name="twitter:description" content="Full-Stack Web Developer &amp; Mentor. I produce elegantly simple things, and I love what I do.">
     <meta name="twitter:image" content="{{ config('app.url') }}/img/og_thumbnail.png">
     <meta name="twitter:image:alt" content="{{ config('app.name') }}">
 
