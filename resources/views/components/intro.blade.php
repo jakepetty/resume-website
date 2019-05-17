227c7f320f0a7bbc0f55bfe51139e0657f55a347
Jake Petty <support@vexcon.net>
2019-05-17 12:26:28 -0500

    Cleaned up Hero

--- resources/views/components/intro.blade.php
+++ resources/views/components/intro.blade.php
@@ -2,7 +2,7 @@
     <div class="middle">
         <header class="animated flipInX">
             <h1>{{ __('My name is') }} {{ config('app.name') }}</h1>
-            <p>{{ __('and I am a { Full-Stack } Web Developer') }}</p>
+            <p>{{ __('I am a { Full-Stack } Web Developer') }}</p>
         </header>
     </div>
 </section>
