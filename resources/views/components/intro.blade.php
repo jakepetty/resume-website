5b1e9d67b601ac332b6c86901abc778617a89ede
Jake Petty <support@vexcon.net>
2019-06-11 11:03:37 -0500

    Updated Hero

--- resources/views/components/intro.blade.php
+++ resources/views/components/intro.blade.php
@@ -1,6 +1,7 @@
 <section id="intro">
     <div class="middle">
         <header class="animated flipInX">
+            <div class="image"></div>
             <h1>{{ __('My name is') }} {{ config('app.name') }}</h1>
             <p>{{ __('I am a { Full-Stack } Web Developer') }}</p>
         </header>
